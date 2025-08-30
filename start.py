a17baeafd55ecaa93772931eff25eaac6f7c7f90
Zifan Si <siz@mcmaster.ca>
2025-08-30 00:45:32 -0400

    Add start

--- start.py
+++ start.py
@@ -1,8 +1,22 @@
+# start.py
 import sys, json
 from models.pattern.apriori import apriori
+from models.pattern.fp_growth import fpgrowth
+from models.pattern.hmine import hmine_mine
 
-if __name__ == "__main__":
-    file, minsup, minconf = sys.argv[2], float(sys.argv[3]), float(sys.argv[4])
+if __name__=="__main__":
+    algo, file = sys.argv[1], sys.argv[2]
     with open(file) as f: tx = json.load(f)
-    freq, rules = apriori([set(t) for t in tx], minsup, minconf)
-    print(freq, rules)
+
+    if algo=="apriori":
+        minsup, minconf = float(sys.argv[3]), float(sys.argv[4])
+        freq, rules = apriori([set(t) for t in tx], minsup, minconf)
+        print(freq, rules)
+    elif algo=="fp":
+        minsup = float(sys.argv[3])
+        print(fpgrowth(tx, minsup))
+    elif algo=="hmine":
+        minsup = float(sys.argv[3])
+        print(hmine_mine(tx, minsup))
+    else:
+        print("unknown algo")
