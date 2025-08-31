a4b8e93e9ef653a0b40fad71f15ae270b40f4ab0
Zifan Si <siz@mcmaster.ca>
2025-08-30 23:52:50 -0400

    Add a runtime counter

--- start.py
+++ start.py
@@ -1,5 +1,4 @@
-# start.py
-import sys, json
+import sys, json, time
 from models.pattern.apriori import apriori
 from models.pattern.fp_growth import fpgrowth
 from models.pattern.hmine import hmine_mine
@@ -7,6 +6,8 @@ from models.pattern.hmine import hmine_mine
 if __name__=="__main__":
     algo, file = sys.argv[1], sys.argv[2]
     with open(file) as f: tx = json.load(f)
+
+    start = time.time()
 
     if algo=="apriori":
         minsup, minconf = float(sys.argv[3]), float(sys.argv[4])
@@ -20,3 +21,5 @@ if __name__=="__main__":
         print(hmine_mine(tx, minsup))
     else:
         print("unknown algo")
+
+    print(f"Runtime: {time.time()-start:.4f} seconds")
